062d95c57f4e2470e41d27d85943b86af0d27859
shreshth-29 <77191933+shreshth-29@users.noreply.github.com>
2022-11-30 23:40:04 +0530

    Add files via upload

---
+++ test_fuzzy_pid.m
@@ -0,0 +1,136 @@
+%% Testing phase, simulating the system using the values of P obtained after training %%
+clc;
+clear all;
+
+%Initializations
+time_step=0.01;
+time_span=100;
+t=0:time_step:time_span;
+init_conditions_train = [0.01 0.04 0.03 -0.06]'; %Initial conditions for training
+init_conditions_test = [0.0 0.00 0.00 0.00]'; %Initial conditions for testing
+count=1; %Time instant
+Xsys=init_conditions_test;
+learning_rate=0.8;
+Kp=10;
+Ki=0.0;
+Kd=0;
+k=0.7;
+
+%System parameters 
+K=7;
+A=[0 1 0 0;0 0 0 0;0 0 0 1;0 0 0 0];
+B=[0 0;K 0;0 0;0 K];
+
+%Getting the appropriate weight matrices after training
+[p1,p2]=train_fuzzy(learning_rate,time_step,5*time_span,A,B,init_conditions_train)
+
+%% Testing %%
+
+for Tinst=0:time_step:time_span
+   
+    T(count)=Tinst;
+    [desiredxVal,desiredyVal,desiredvxVal,desiredvyVal]= desiredtrajpoint(Tinst); %Desired position and velocity
+    
+    %Position and velocity errors
+    ErrorX(count) = desiredxVal-Xsys(1);
+    IntErrorX(count) = sum(ErrorX);
+    ErrorY(count) = desiredyVal-Xsys(3);
+    IntErrorY(count) = sum(ErrorY);
+    vErrorX(count) = desiredvxVal-Xsys(2);
+    vErrorY(count) = desiredvyVal-Xsys(4);
+
+    loss_p(count)=0.5*(ErrorX(count))^2+0.5*(ErrorY(count))^2;
+    loss_v(count)=0.5*(vErrorX(count))^2+0.5*(vErrorY(count))^2;
+
+    desiredAlpha=desiredxVal/7;
+    desiredBeta=4*desiredyVal/7;
+
+    %Calculate alpha
+    X=[1;ErrorX(count); vErrorX(count)];
+    F1=p1*X;
+    W1=NeuroFuzzy(ErrorX(count),vErrorX(count));
+    alphaFuzzy=W1'*F1;
+    alphaPID=Kp*(ErrorX(count))+Kp*(vErrorX(count));
+    alpha=k*alphaFuzzy+(2-k)*alphaPID;
+    
+    %Calculate beta
+    Y=[1;ErrorY(count); vErrorY(count)];
+    F2=p2*Y;
+    W2=NeuroFuzzy(ErrorY(count),vErrorY(count));
+    betaFuzzy=W2'*F2;
+    betaPID=Kp*(ErrorY(count))+Kp*(vErrorY(count));
+    beta=k*betaFuzzy+(2-k)*betaPID;
+
+    %Update system based on calculated alpha and beta
+    dx=(A*Xsys)+B*[alpha;beta];
+    Xsys=Xsys+(dx*time_step);
+
+    Xtraj(count,:)=Xsys;
+    desired(1)=desiredxVal;
+    desired(2)=desiredyVal;
+    desired(3)=desiredvxVal;
+    desired(4)=desiredvyVal;
+
+    Dtraj(count,:)=desired; %Storing the desired trajectory points
+    input(count , :)=[alpha,beta]; %storing control input values, i.e alpha,beta
+    count=count+1;
+    
+end
+
+rmse_p=sqrt(sum(loss_p)/length(loss_p))
+rmse_v=sqrt(sum(loss_v)/length(loss_v))
+
+figure(1) %Desired and actual trajectory vs time
+plot(Xtraj(1:4:3001,1),Xtraj(1:4:3001,3),'bo-','LineWidth',0.1)
+hold on
+plot(Dtraj(1:4:3001,1), Dtraj(1:4:3001,2),'r--','LineWidth',2)
+hold on
+%axis('square');
+title('Trajectory plot');
+xlabel('X axis');
+h_xlabel = get(gca,'XLabel');
+set(h_xlabel,'FontSize',20);
+ylabel('Y axis');
+h_ylabel = get(gca,'XLabel');
+set(h_ylabel,'FontSize',20);
+set(gca,'FontSize',12);
+legend('Actual Trajectory','Desired Trajectory');
+grid on;
+
+figure(2) %Control inputs (alpha and beta) vs time
+plot(1:count-1,input(:,2),'b-','LineWidth',2)
+hold on
+plot(1:count-1,input(:,1),'ro','LineWidth',2)
+%axis('square');
+title('Input Angles vs time');
+xlabel('Angles');
+h_xlabel = get(gca,'XLabel');
+set(h_xlabel,'FontSize',20);
+ylabel('Time');
+h_ylabel = get(gca,'XLabel');
+set(h_ylabel,'FontSize',20);
+set(gca,'FontSize',12);
+legend('Beta','Alpha');
+grid on;
+
+% figure(3)
+% plot(1:count-1,ErrorY)
+
+
+%% Finding the appropriate position and velocity based on the lissajous trajectory %%
+function [desiredxVal,desiredyVal,desiredvxVal,desiredvyVal]= desiredtrajpoint(t)
+    A=0.04;
+    B=0.04;
+    a=2.5;
+    b=1;
+    delta=pi/2;
+    desiredxVal=A*sin((a*t)+delta);
+    desiredyVal=B*sin(b*t);
+    desiredvxVal=A*a*cos((a*t)+delta);
+    desiredvyVal=B*b*cos(b*t);
+%     desiredxVal=A*cos(t)*(1-cos(t));
+%     desiredyVal=B*sin(t)*(1-cos(t));
+%     desiredvxVal=A*sin(t)*(2*cos(t) -1);
+%     desiredvyVal=B*((sin(t))^2-(cos(t))^2 + cos(t));
+end
+
---
+++ train_fuzzy.m
@@ -0,0 +1,122 @@
+%% Training phase for finding the weight matrices P for alpha and beta %%
+
+function[p1,p2]=train_fuzzy(learning_rate,time_step,time_span,A,B,init_condition)
+
+    rand('seed',1) %setting the seed
+
+    %Randomly initializing the weight matrices
+    p2=rand(25,3);
+    p1=rand(25,3);
+
+    t=0:time_step:time_span;
+    count=1; %time instants
+    Xsys=init_condition;
+
+    dw_prev_1=0;
+    dw_curr_1=0;
+    dw_prev_2=0;
+    dw_curr_2=0;
+
+    gamma=0.95;
+    
+    for Tinst=0:time_step:time_span
+        T(count)=Tinst;
+        [desiredxVal,desiredyVal,desiredvxVal,desiredvyVal]= desiredtrajpoint(Tinst);
+        desiredAlpha=desiredxVal/7;
+        desiredBeta=4*desiredyVal/7;
+        
+        %Position and velocity errors
+        ErrorX(count) = desiredxVal-Xsys(1);
+        ErrorY(count) = desiredyVal-Xsys(3);
+        vErrorX(count) = desiredvxVal-Xsys(2);
+        vErrorY(count) = desiredvyVal-Xsys(4);
+
+        DesiredAlphaTraj(count)=desiredAlpha;
+        DesiredBetaTraj(count)=desiredBeta;
+        
+        %Control in the x direction
+        X=[1;ErrorX(count); vErrorX(count)]; %input to fuzzy logic controller
+        F1=p1*X;
+        W1=NeuroFuzzy(ErrorX(count),vErrorX(count)); %weights based on the fuzzy logic controller
+        z1=W1'*F1;
+        alpha=tansig(z1);
+
+        %constants for backpropogation
+        a1=0.01;
+        b1=0.02;
+        lam1=(a1*ErrorX(count)+(b1)*vErrorX(count))*0.5*(1+alpha)*(1-alpha);
+        for i=1:25
+            for j=1:3
+                if j==1
+                    dp1=W1(i)*lam1*1;
+                elseif j==2
+                    dp1=W1(i)*lam1*ErrorX(count);
+                else 
+                    dp1=W1(i)*lam1*vErrorX(count);
+                end
+                %Weight update using gradient descent, with gradient
+                %estimated from backpropogation
+                dw_curr_1=learning_rate*dp1;
+                p1(i,j)=p1(i,j)+ (gamma)*dw_curr_1+ (1-gamma)*dw_prev_1;
+                dw_prev_1=dw_curr_1;
+            end
+        end
+    
+        %Control in the y direction
+        Y=[1;ErrorY(count); vErrorY(count)];
+        F2=p2*Y;
+        W2=NeuroFuzzy(ErrorY(count),vErrorY(count));
+        z2=W2'*F2;
+        beta=tansig(z2);
+        %update weights
+        a2=0.01;
+        b2=0.02;
+        lam2=(a2*ErrorY(count))+((b2)*vErrorY(count))*0.5*(1+beta)*(1-beta);
+        for i=1:25
+            for j=1:3
+                if j==1
+                    dp2=W2(i)*lam2*1;
+                elseif j==2
+                    dp2=W2(i)*lam2*ErrorY(count);
+                else 
+                    dp2=W2(i)*lam2*vErrorY(count);
+                end
+                %Weight update using gradient descent, with gradient
+                %estimated from backpropogation
+                dw_curr_2=learning_rate*dp2;
+                p2(i,j)=p2(i,j)+ (gamma)*dw_curr_2+ (1-gamma)*dw_prev_2;
+                dw_prev_2=dw_curr_2;
+                
+            end
+        end
+
+        %Update system based on newly calculated alpha and beta
+        dx=(A*Xsys)+B*[alpha;beta];
+        Xsys=Xsys+(dx*time_step);
+        Xtraj(count,:)=Xsys;
+
+        desired(1)=desiredxVal;
+        desired(2)=desiredyVal;
+        desired(3)=desiredvxVal;
+        desired(4)=desiredvyVal;
+        Dtraj(count,:)=desired;
+        
+        %go to next time instant
+        count=count+1;
+    end
+    
+end
+
+
+%% Finding the appropriate position and velocity based on the lissajous trajectory %%
+function [desiredxVal,desiredyVal,desiredvxVal,desiredvyVal]= desiredtrajpoint(t)
+    A=0.04;
+    B=0.04;
+    a=1;
+    b=2;
+    delta=pi/2;
+    desiredxVal=A*sin((a*t)+delta);
+    desiredyVal=B*sin(b*t);
+    desiredvxVal=A*a*cos((a*t)+delta);
+    desiredvyVal=B*b*cos(b*t);
+end
